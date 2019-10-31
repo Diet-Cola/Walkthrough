4cde1a2f453d1cf21dfa2b32e35f9f9c9956b930
Diet-Cola <vaultslave@gmail.com>
2019-10-31 00:36:34 +0000

    Updated

--- src/main/java/com/github/diet/Walkthrough.java
+++ src/main/java/com/github/diet/Walkthrough.java
@@ -2,16 +2,12 @@ package com.github.diet;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 
 import com.github.diet.listeners.PlayerListener;
 import com.github.diet.utils.WalkthroughSettingsManager;
 
 import hu.trigary.advancementcreator.Advancement;
 import hu.trigary.advancementcreator.AdvancementFactory;
-import hu.trigary.advancementcreator.shared.ItemObject;
-import hu.trigary.advancementcreator.trigger.ImpossibleTrigger;
-import net.md_5.bungee.api.chat.TextComponent;
 import vg.civcraft.mc.civmodcore.ACivMod;
 
 public class Walkthrough extends ACivMod {
@@ -35,18 +31,15 @@ public class Walkthrough extends ACivMod {
 			getLogger().info("CivModCore not enabled, shutting down");
 			Bukkit.shutdown();
 		}
-		getLogger().info("Walkthrough enabled");
 		//Registering all advancements
-		AdvancementFactory factory = new AdvancementFactory(this, true, false);
-		Advancement root = factory.getRoot("main/root", "Welcome to CivClassic!", "You've joined for the first time", Material.GRASS, "gui/advancements/backgrounds/stone");
-		new Advancement(new NamespacedKey(this, "main/localChat"), new ItemObject().setItem(Material.BOOK),
-                new TextComponent("Only few can hear you..."), new TextComponent("Chat range is limited to 1,000 blocks here."))
-                .addTrigger("impossible", new ImpossibleTrigger())
-                .makeChild(root.getId())
-                .setFrame(Advancement.Frame.GOAL)
-                .activate(false);
+		AdvancementFactory factory = new AdvancementFactory(Walkthrough.getInstance(), true, false);
+		Advancement mainRoot = factory.getRoot("main/root", "Welcome to CivClassic!", "You've joined for the first time", Material.GRASS, "gui/advancements/backgrounds/stone");
+		Advancement localChat = factory.getImpossible("main/localChat", mainRoot, "Only few can hear you...", "Chat range is limited to 1,000 blocks here.", Material.BOOK);
+		Advancement getWood = factory.getImpossible("main/getWood", localChat, "Things grow differently here", "Trees and crops grow differently by biome. Try right clicking the ground with a sapling.", Material.OAK_LOG);
+		Advancement goMining = factory.getImpossible("main/goMining", localChat, "You found an ore!", "Ore generates as you mine stone!", Material.DIAMOND_ORE);
 		registerListener(new PlayerListener());
 		settingsManager = new WalkthroughSettingsManager();
+		getLogger().info("Walkthrough enabled");
 		//Required to stop advancements overlapping each other.
 		Bukkit.reloadData();
 	}
